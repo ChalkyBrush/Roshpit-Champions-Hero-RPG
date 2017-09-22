4ae3f2ac15a8c0cd4bcbc614fbdb0593c751480a
Sergey Demchenko <rutelun@ya.ru>
2017-09-23 00:56:53 +0300

    update builder

--- roshpit_npc_builder.py
+++ roshpit_npc_builder.py
@@ -1,13 +1,22 @@
 import os.path
+settings = {}
 
+def parse_settings(file_path):
+    replaces = {}
+    file = open(file_path, 'r')
+    for line in file:
+        temp = line.split(':')
+        replaces[temp[0].strip()] = temp[1].strip()
+    file.close()
+    return replaces
 
 def parse_replaces(file_path):
     replaces = {}
     file = open(file_path, 'r')
     for line in file:
         temp = line.split(':')
-        temp_file = open(temp[1], 'r')
-        replaces['##' + temp[0] + '##'] = temp_file.read()
+        temp_file = open(settings['base_path'] + temp[1].strip(), 'r')
+        replaces['##' + temp[0].strip() + '##'] = temp_file.read()
         temp_file.close()
     file.close()
     return replaces
@@ -15,7 +24,7 @@ def parse_replaces(file_path):
 
 def replace_in_file(input_file_patch, output_file_path, replaces):
     input_file = open(input_file_patch, 'r')
-    output_file = open(output_file_path, 'w')
+    output_file = open(settings['output_path'] + output_file_path, 'w')
     for line in input_file:
         if replaces.get(line.strip()):
             output_file.write(replaces.get(line.strip()))
@@ -24,6 +33,7 @@ def replace_in_file(input_file_patch, output_file_path, replaces):
     input_file.close()
     output_file.close()
 
+settings = parse_settings('builder_settings.txt')
 
 if os.path.isfile('npc_abilities_custom_template.txt') and os.path.isfile('npc_abilities_custom_replace.txt'):
     abilities_replace = parse_replaces('npc_abilities_custom_replace.txt')
