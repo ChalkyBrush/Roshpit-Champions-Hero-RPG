015c2bfdff98e540fe3a7d92aae49ceb0d96319b
Sergey Demchenko <rutelun@ya.ru>
2017-10-24 17:16:12 +0300

    build/now watch datadriven and constants files for autorebuild

--- roshpit_npc_builder.py
+++ roshpit_npc_builder.py
@@ -1,6 +1,80 @@
+from time import sleep
+from time import gmtime, strftime
 import os.path
 settings = {}
 settings['constants'] = {}
+settings['parse_groups'] = [
+    {
+        "template": 'npc_abilities_custom_template.txt',
+        "replace": 'npc_abilities_custom_replace.txt',
+        "output_file": 'npc_abilities_custom.txt',
+        "finish_label": 'abilities done'
+    },
+    {
+        "template": 'npc_items_custom_template.txt',
+        "replace": 'npc_items_custom_replace.txt',
+        "output_file": 'npc_items_custom.txt',
+        "finish_label": 'items done'
+    },
+    {
+        "template": 'npc_units_custom_template.txt',
+        "replace": 'npc_units_custom_replace.txt',
+        "output_file": 'npc_units_custom.txt',
+        "finish_label": 'units done'
+    }
+]
+
+filesChangeTime = {}
+using_files = []
+
+
+def build():
+    global settings
+    print('---- build time ' + strftime("%Y-%m-%d %H:%M:%S", gmtime()) + ' ----')
+    for parse_group in settings['parse_groups']:
+        if not (os.path.isfile(parse_group["template"]) and os.path.isfile(parse_group["replace"])):
+            raise Exception("File " + parse_group["template"] + " or file " + parse_group["replace"] + " does not exist")
+        local_replace = parse_replaces(parse_group["replace"])
+        replace_in_file(parse_group["template"], parse_group["output_file"], local_replace)
+        print(parse_group["finish_label"])
+
+
+def watch():
+    global settings
+    global using_files
+    while True:
+        should_rebuild = False
+        already_builded = False
+        for parse_group in settings['parse_groups']:
+            if is_file_changed(parse_group["template"]):
+                should_rebuild = True
+            if is_file_changed(parse_group["replace"]):
+                should_rebuild = True
+        if should_rebuild:
+            using_files = []
+            build()
+            already_builded = True
+        for file in using_files:
+            if is_file_changed(file):
+                should_rebuild = True
+        if should_rebuild and not already_builded:
+            using_files = []
+            build()
+        sleep(1)
+
+
+def is_file_changed(path):  # additionally update modification data
+    global filesChangeTime
+    change_time = os.path.getmtime(path)
+    if path not in filesChangeTime:
+        filesChangeTime[path] = change_time
+        return True
+    elif change_time > filesChangeTime[path]:
+        filesChangeTime[path] = change_time
+        return True
+    else:
+        return False
+
 
 def parse_constants(file_path):
     constants = {}
@@ -11,7 +85,7 @@ def parse_constants(file_path):
         if '=' not in line:
             continue
         temp = line.split('=')
-        constants[temp[0].strip()] = temp[1].strip().replace("'","").replace('"', '')
+        constants[temp[0].strip()] = temp[1].strip().replace("'", "").replace('"', '')
     return constants
 
 
@@ -24,17 +98,22 @@ def parse_settings(file_path):
     file.close()
     return replaces
 
+
 def parse_replaces(file_path):
     replaces = {}
     current_constants = {}
     file = open(file_path, 'r')
     for line in file:
         if '<%' in line:
-            line = line.replace('<% ','').replace(' %>','').strip()
+            line = line.replace('<% ','').replace(' %>', '').strip()
+            using_files.insert(1, settings['base_path'] + line)
             current_constants = parse_constants(settings['base_path'] + line)
+            continue
+        if ':' not in line:
             continue
         temp = line.split(':')
         addition_file_path = settings['base_path'] + temp[1].strip()
+        using_files.insert(1, addition_file_path)
         replaces['##' + temp[0].strip() + '##'] = prepare_file(addition_file_path, current_constants)
     file.close()
     return replaces
@@ -62,17 +141,6 @@ def prepare_file(file_path, constants):
         raise Exception("Constants " + constant_name + " in " + file_path + " don't parsed. There should be space after <% and before %>")
     return content
 
-settings = parse_settings('builder_settings.txt')
 
-if os.path.isfile('npc_abilities_custom_template.txt') and os.path.isfile('npc_abilities_custom_replace.txt'):
-    abilities_replace = parse_replaces('npc_abilities_custom_replace.txt')
-    replace_in_file('npc_abilities_custom_template.txt', 'npc_abilities_custom.txt', abilities_replace)
-    print('abilities done')
-if os.path.isfile('npc_items_custom_template.txt') and os.path.isfile('npc_items_custom_replace.txt'):
-    items_replace = parse_replaces('npc_items_custom_replace.txt')
-    replace_in_file('npc_items_custom_template.txt', 'npc_items_custom.txt', items_replace)
-    print('items done')
-if os.path.isfile('npc_units_custom_template.txt') and os.path.isfile('npc_units_custom_replace.txt'):
-    units_replace = parse_replaces('npc_units_custom_replace.txt')
-    replace_in_file('npc_units_custom_template.txt', 'npc_units_custom.txt', units_replace)
-    print('units done')
+settings.update(parse_settings('builder_settings.txt'))
+watch()
