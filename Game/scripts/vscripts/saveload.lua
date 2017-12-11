265a72f8a3dd402b64e842891dc55a990887a6ae
Ryan Racioppo <ryanracioppo@gmail.com>
2017-12-10 21:40:06 -0500

    -small fix

--- Game/scripts/vscripts/saveload.lua
+++ Game/scripts/vscripts/saveload.lua
@@ -64,7 +64,6 @@ end
 
 function SaveLoad:ProcessedKey(msg)
 	SaveLoad.key2 = msg.number
-	print(SaveLoad.key2)
 	if SaveLoad:GetAllowSaving() and alert then
 		CustomGameEventManager:Send_ServerToAllClients("server_confirmed", {} )
 	end
