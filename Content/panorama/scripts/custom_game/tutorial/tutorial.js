20598b2afd45f00009da69853a8907dca38a3544
Ryan Racioppo <ryanracioppo@gmail.com>
2018-09-08 19:36:27 -0400

    more tutorial ui

--- Content/panorama/scripts/custom_game/tutorial/tutorial.js
+++ Content/panorama/scripts/custom_game/tutorial/tutorial.js
@@ -1,4 +1,5 @@
 TUTORIAL_CATEGORIES = 2
+mCloseButton = null
 
 function OpenTutorial(msg){
 	var parent = $('#tutorial_container')
@@ -15,9 +16,13 @@ function OpenTutorial(msg){
     var imageName = "file://{images}/custom_game/ui/tutorial/tutorial_header.jpg"
     header.SetImage(imageName)
 
-	for (var i = 0; i < TUTORIAL_CATEGORIES; i++) {
+
+    mCloseButton = tutorial_main.FindChildTraverse('close_button')
+    mCloseButton.FindChildTraverse('close_button_label').text = $.Localize("ui_close")
+    var available_category_count = Object.keys(msg.categories).length
+	for (var i = 0; i < available_category_count; i++) {
 		var category = msg.categories[i+1]
-	    setupCategory(category, i, tutorial_main.FindChildTraverse('tutorial_categories_container'))
+	    setupCategory(category, i, tutorial_main.FindChildTraverse('tutorial_categories_container'), false, msg)
 	}
    
 	tutorial_main.FindChildTraverse('close_button').SetPanelEvent('onactivate', function Close() {
@@ -35,20 +40,55 @@ function CloseTutorial(msg){
 	parent.RemoveClass('animateEaseOutClass')
 }
 
-function setupCategory(category, index, parent)
+function setupCategory(category, index, parent, bStatic, msg)
 {
 	var categoryPanel = $.CreatePanel("Panel", parent, "category"+index)
 	categoryPanel.BLoadLayoutSnippet("tutorial_category")
 	categoryPanel.FindChildTraverse('tutorial_category_header_label').text = $.Localize(category["header"])
 	categoryPanel.FindChildTraverse('tutorial_category_description_label').text = $.Localize(category["description"])
-	categoryPanel.SetPanelEvent('onmouseover', function Close() {
-		categoryPanel.FindChildTraverse('tutorial_category_header').AddClass('tutorial_category_header_active')
-		categoryPanel.AddClass('tutorial_category_description_active')
-	});
-	categoryPanel.SetPanelEvent('onmouseout', function Close() {
-		categoryPanel.FindChildTraverse('tutorial_category_header').RemoveClass('tutorial_category_header_active')
-		categoryPanel.RemoveClass('tutorial_category_description_active')
-	});
+	var categoryButton = categoryPanel.FindChildTraverse('tutorial_category_clickable')
+	if (!(bStatic)){
+		categoryPanel.SetPanelEvent('onmouseover', function HoverIn() {
+			categoryPanel.FindChildTraverse('tutorial_category_header').AddClass('tutorial_category_header_active')
+			categoryPanel.AddClass('tutorial_category_description_active')
+		});
+		categoryPanel.SetPanelEvent('onmouseout', function HoverOut() {
+			categoryPanel.FindChildTraverse('tutorial_category_header').RemoveClass('tutorial_category_header_active')
+			categoryPanel.RemoveClass('tutorial_category_description_active')
+		});
+		categoryButton.SetPanelEvent('onactivate', function Activate() {
+			category_panel_click_setup(categoryPanel, index, category, msg)
+		});
+	}
+}
+
+function category_panel_click_setup(categoryPanel, index, category, msg){
+	var parent = $('#tutorial_container')
+	var category_container = parent.FindChildTraverse('tutorial_categories_container')
+	category_container.RemoveAndDeleteChildren(0)
+	setupCategory(category, index, category_container, true)
+	var challengeListPanel = $.CreatePanel("Panel", category_container, "challenge-list")
+	challengeListPanel.BLoadLayoutSnippet('tutorial_challenge_list')
+	var challengeCount = Object.keys(category.challenges).length
+	for (var i = 0; i < challengeCount; i++) {
+		setupChallenge(category, category.challenges[i+1], index, challengeListPanel)
+	}
+	mCloseButton.FindChildTraverse('close_button_label').text = $.Localize("ui_back")
+	mCloseButton.SetPanelEvent('onactivate', function Back() {
+		OpenTutorial(msg)
+	})
+}
+
+function setupChallenge(category, challenge, index, challengeListPanel){
+	$.Msg(category)
+	$.Msg(category["progress"])
+	if (category["progress"] >= index){
+		var challengePanel = $.CreatePanel("Panel", challengeListPanel, "challenge"+index)
+		challengePanel.BLoadLayoutSnippet("tutorial_challenge")	
+		var quest_number = category["index"]
+		var challenge_number = index + 1
+		challengePanel.FindChildTraverse('tutorial_challenge_text').text = $.Localize('quest_'+quest_number+"_challenge_"+challenge_number) 
+	}
 }
 
 (function()
--- Game/scripts/vscripts/worlds/tutorial/tutorial.lua
+++ Game/scripts/vscripts/worlds/tutorial/tutorial.lua
@@ -227,7 +227,7 @@ function Tutorial:SoundAndAnimationForMaster(sound, animationName, playRate, dur
 end
 
 function Tutorial:TutorialUIActiveForPlayer(hero, sound)
-	local categories = Tutorial:GetFixedTutorialData()
+	local categories = Tutorial:GetFixedTutorialData(hero)
 	local playerID = hero:GetPlayerOwnerID()
 	local player = PlayerResource:GetPlayer(playerID)
 	CustomGameEventManager:Send_ServerToPlayer(player, "open_tutorial", {hero=hero:GetEntityIndex(), tutorial=hero.tutorial, sound=sound, categories=categories} )
@@ -236,10 +236,11 @@ function Tutorial:TutorialUIActiveForPlayer(hero, sound)
 	--uncomment this in before release
 end
 
-function Tutorial:GetFixedTutorialData()
+function Tutorial:GetFixedTutorialData(hero)
 	local categories = {}
 	--
 	local quest = {}
+	quest.progress = hero.tutorial.section1.progress
 	quest.index = 1
 	quest.header = "quest_1_interface"
 	quest.description = "quest_1_interface_description"
@@ -249,14 +250,17 @@ function Tutorial:GetFixedTutorialData()
 	table.insert(categories, quest)
 	--
 	--
-	local quest = {}
-	quest.index = 2
-	quest.header = "quest_2_interface"
-	quest.description = "quest_2_interface_description"
-	quest.challenges = {}
-	local challenge = {}
-	table.insert(quest.challenges, challenge)
-	table.insert(categories, quest)
+	if hero.tutorial.section1.progress >= 8 then
+		local quest = {}
+		quest.index = 2
+		quest.progress = hero.tutorial.section2.progress
+		quest.header = "quest_2_interface"
+		quest.description = "quest_2_interface_description"
+		quest.challenges = {}
+		local challenge = {}
+		table.insert(quest.challenges, challenge)
+		table.insert(categories, quest)
+	end
 	--
 	return categories
 end
