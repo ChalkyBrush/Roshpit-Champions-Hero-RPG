632aede49fc7677e976b6eb41172d949dbb0a25d
GoToNightmare <holdscroll@gmail.com>
2018-09-10 22:55:48 +0500

    axe w3 buff

--- Game/scripts/vscripts/heroes/axe/abilities/w/w3_return.lua
+++ Game/scripts/vscripts/heroes/axe/abilities/w/w3_return.lua
@@ -1,7 +1,8 @@
 local function cast(caster, abilityLevel)
     if caster.w_3_level > 0 then
-        local healAmount = caster.w_3_level* W3_HEAL * abilityLevel
-        Filters:ApplyHeal(caster, caster, healAmount, true)
+        local healAmountFlat = caster.w_3_level * W3_HEAL_FLAT * abilityLevel
+        local healAmountPct = caster.w_3_level * W3_HEAL_PCT / 100 * caster:GetMaxHealth() * abilityLevel 
+        Filters:ApplyHeal(caster, caster, healAmountFlat+healAmountPct, true)
         -- PopupHealing(caster, healAmount)
     end
 end
