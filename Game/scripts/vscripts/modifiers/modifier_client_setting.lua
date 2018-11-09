2ccb11ea5aa8934e76338a2f8098e2b509883824
bavibi <bavibi43@gmail.com>
2018-11-09 22:23:24 +0300

    Interval for reconnecting players

--- Game/scripts/vscripts/modifiers/modifier_client_setting.lua
+++ Game/scripts/vscripts/modifiers/modifier_client_setting.lua
@@ -1,6 +1,10 @@
 modifier_client_setting = class({})
 
 function modifier_client_setting:OnCreated()
+	self:StartIntervalThink(2)
+end
+
+function modifier_client_setting:OnIntervalThink()
 	if IsClient() then
 		SendToConsole("dota_hud_healthbars 1")
 	end
