03203ebdef9e0c3de69da0771300e3ed1aa0b7e4
Ryan Racioppo <ryanracioppo@gmail.com>
2019-01-15 17:51:01 -0500

    cosmic comsic e

---
+++ Game/scripts/vscripts/heroes/arc_warden/abilities/e_cosmic_cosmic.lua
@@ -0,0 +1,36 @@
+function jex_cosmic_cosmic_e_phase_start(event)
+	local caster = event.caster
+	CustomAbilities:QuickAttachParticle("particles/econ/items/faceless_void/faceless_void_jewel_of_aeons/fv_time_walk_jewel.vpcf", caster, 1.5)
+	StartAnimation(caster, {duration=1, activity=ACT_DOTA_OVERRIDE_ABILITY_4, rate=0.6})
+	EmitSoundOn("Jex.Warp.Pre", caster)
+	EmitSoundOn("Jex.Warp.Activate", caster)
+end
+
+function jex_cosmic_port_start(event)
+	local caster = event.caster
+	local ability = event.ability
+	local target = event.target_points[1]
+	
+
+    local casterOrigin = caster:GetAbsOrigin()
+    target = WallPhysics:WallSearch(casterOrigin, target, caster)
+    local newPosition = target
+    FindClearSpaceForUnit(caster, newPosition, false)
+    Filters:CastSkillArguments(3, caster)
+    ProjectileManager:ProjectileDodge(caster)
+
+    if caster:HasModifier("modifier_jex_warp_freecast") then
+    	local stacks = caster:GetModifierStackCount("modifier_jex_warp_freecast", caster)
+    	if stacks > 1 then
+    		caster:SetModifierStackCount("modifier_jex_warp_freecast", caster, stacks - 1)
+    	else
+    		caster:RemoveModifierByName("modifier_jex_warp_freecast")
+    	end
+    	ability:EndCooldown()
+    else
+    	local tech_level = caster.onibi.stats_table["cosmic"]["cosmic"]["E"]["level"]
+    	tech_level = 5
+    	ability:ApplyDataDrivenModifier(caster, caster, "modifier_jex_warp_freecast", {})
+    	caster:SetModifierStackCount("modifier_jex_warp_freecast", caster, tech_level)
+    end
+end
--- Game/scripts/vscripts/heroes/arc_warden/abilities/onibi.lua
+++ Game/scripts/vscripts/heroes/arc_warden/abilities/onibi.lua
@@ -180,6 +180,21 @@ function get_ability_name_by_element_combination_and_key(element1, element2, abi
 			ability_name = "jex_cosmic_cosmic_w"
 		end
 	elseif ability_key == "E" then
+		if element1 == "nature" and element2 == "nature" then
+			ability_name = ""
+		elseif (element1 == "nature" and element2 == "lightning") or (element1 == "lightning" and element2 == "nature") then
+			ability_name = ""
+		elseif (element1 == "nature" and element2 == "cosmic") or (element1 == "cosmic" and element2 == "nature") then
+			ability_name = ""
+		elseif element1 == "lightning" and element2 == "lightning" then
+			ability_name = ""
+		elseif (element1 == "lightning" and element2 == "cosmic") or (element1 == "cosmic" and element2 == "lightning") then
+			ability_name = ""
+		elseif element1 == "cosmic" and element2 == "cosmic" then
+			ability_name = "jex_cosmic_cosmic_e"
+		end
+
+
 	end
 	return ability_name
 end
