317781635e1e79b62a8e2f985b296904c3b6cadc
GoToNightmare <holdscroll@gmail.com>
2019-01-18 00:31:12 +0500

    removed cd check and more time for baha boss spell

--- Game/scripts/vscripts/worlds/sea_fortress/sea_fortress_ai.lua
+++ Game/scripts/vscripts/worlds/sea_fortress/sea_fortress_ai.lua
@@ -6027,8 +6027,8 @@ function shadow_of_bahamut_think(event)
 
 			end	
 			for j = 1, #ability.blastTable, 1 do
-				CustomAbilities:QuickParticleAtPoint("particles/roshpit/seafortress/shadow_of_bahamut_indicator_portrait.vpcf", GetGroundPosition(ability.blastTable[j], caster) + Vector(0,0,10), 1.2)
-				Timers:CreateTimer(1.1, function()
+				CustomAbilities:QuickParticleAtPoint("particles/roshpit/seafortress/shadow_of_bahamut_indicator_portrait.vpcf", GetGroundPosition(ability.blastTable[j], caster) + Vector(0,0,10), 2.2)
+				Timers:CreateTimer(2.1, function()
 					local enemies = FindUnitsInRadius( caster:GetTeamNumber(), ability.blastTable[j], nil, 380, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, FIND_ANY_ORDER, false )
 					for i = 1, #enemies, 1 do
 						enemies[i]:ForceKill(false)
