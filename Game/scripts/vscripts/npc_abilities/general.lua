6cfb7f656d62f451bf4918368186be62c60bcf65
GoToNightmare <holdscroll@gmail.com>
2019-01-18 00:25:06 +0500

    removed data-driven thinkers

--- Game/scripts/vscripts/npc_abilities/general.lua
+++ Game/scripts/vscripts/npc_abilities/general.lua
@@ -324,7 +324,16 @@ function ability_1_no_target_ai(event)
 		return false
 	end
 	local radius = caster.targetRadius
+	if not radius or type(radius) ~= "float" then
+		radius = 450
+	end
 	local castAbility = caster:GetAbilityByIndex(0)
+	if not caster.autoAbilityCD then
+		caster.aggro = true
+		caster.autoAbilityCD = 1
+		print("ability_1_no_target_ai caster.autoAbilityCD")
+		return
+	end	
 	local cooldown = caster.autoAbilityCD*2
 	if caster.interval%cooldown == 0 and caster.aggro then
 		if castAbility:IsFullyCastable() then
@@ -362,7 +371,16 @@ function ability_1_position_think_generic(event)
 	local castAbility = caster:GetAbilityByIndex(0)
 	
 	local radius = caster.targetRadius
+	if not radius or type(radius) ~= "float" then
+		radius = 450
+	end	
 	local minRadius = caster.minRadius
+	if not caster.targetAbilityCD then
+		caster.aggro = true
+		caster.targetAbilityCD = 1
+		print("ability_1_position_think_generic caster.targetAbilityCD")
+		return
+	end
 	local cooldown = caster.targetAbilityCD*2
 	local targetFindOrder = caster.targetFindOrder
 	if caster.interval%cooldown == 0 and caster.aggro then
