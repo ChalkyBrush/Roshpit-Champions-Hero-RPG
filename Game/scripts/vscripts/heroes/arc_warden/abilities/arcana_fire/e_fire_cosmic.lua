a2c54af451331b84af39a74bdf86e21c6c7fc2f5
Ryan Racioppo <ryanracioppo@gmail.com>
2019-03-07 17:40:57 -0500

    fix cipher bolt

--- Game/scripts/vscripts/heroes/arc_warden/abilities/arcana_fire/e_fire_cosmic.lua
+++ Game/scripts/vscripts/heroes/arc_warden/abilities/arcana_fire/e_fire_cosmic.lua
@@ -126,6 +126,6 @@ function cipher_bolt_burn_think(event)
 	local caster = event.caster
 	local ability = event.ability
 	local target = event.target
-	local damage = (event.w_4_burn_damage_attack_power*ability.w_4_level/100)
+	local damage = (event.w_4_burn_damage_attack_power*ability.w_4_level/100)*OverflowProtectedGetAverageTrueAttackDamage(caster)
 	Filters:ApplyDotDamage(caster, ability, target, damage, DAMAGE_TYPE_MAGICAL, 3, RPC_ELEMENT_FIRE, RPC_ELEMENT_NONE)
 end
