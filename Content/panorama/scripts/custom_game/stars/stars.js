ed1b2ca7ecd408ffc147087a54deff0c698ac24e
Ryan Racioppo <ryanracioppo@gmail.com>
2019-05-06 18:26:39 -0400

    max stars

--- Content/panorama/scripts/custom_game/stars/stars.js
+++ Content/panorama/scripts/custom_game/stars/stars.js
@@ -9,7 +9,7 @@ mActiveCategoryButton = false
 mButtonRows = 3
 mButtonsPerRow = 10
 TOTAL_HERO_CATEGORIES = 12
-MAX_STARS = 1206
+MAX_STARS = 1242
 
 function openStarsFromServer(msg){
 	$('#stars_container').RemoveClass('invisible')
