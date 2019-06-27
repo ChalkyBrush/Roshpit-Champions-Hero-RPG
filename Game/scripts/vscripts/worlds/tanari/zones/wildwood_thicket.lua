60a3156708f93a4fd2f6e16941712527d7b47da8
Nintai Nousatsu <nintai@hotmail.de>
2019-06-27 15:27:55 +0200

    Formatted Tanari

--- Game/scripts/vscripts/worlds/tanari/zones/wildwood_thicket.lua
+++ Game/scripts/vscripts/worlds/tanari/zones/wildwood_thicket.lua
@@ -2,8 +2,8 @@ function Tanari:SpawnThicket()
 	-- AddFOWViewer(DOTA_TEAM_GOODGUYS, Vector(7493, 3584, 500), 8000, 1000, false)
 	--DEBUG
 	local thicketPoint1 = Vector(6208, 4864)
-	local thicketPoint2 = Vector(9472, 4480) 
-	local thicketPoint3 = Vector(7168, 4032) 
+	local thicketPoint2 = Vector(9472, 4480)
+	local thicketPoint3 = Vector(7168, 4032)
 	local thicketPoint4 = Vector(6492, 2752)
 	local thicketPoint5 = Vector(7872, 1536)
 	local thicketPoint6 = Vector(8022, 5211)
@@ -14,55 +14,55 @@ function Tanari:SpawnThicket()
 	Timers:CreateTimer(3, function()
 		Timers:CreateTimer(8, function()
 			local patrolHunter1 = Tanari:SpawnHeadhunter(thicketPoint1)
-			local patrolHunter2 = Tanari:SpawnHeadhunter(thicketPoint1+RandomVector(100))
+			local patrolHunter2 = Tanari:SpawnHeadhunter(thicketPoint1 + RandomVector(100))
 			patrolHunter1.patrolPositionTable = {thicketPoint2, thicketPoint5, thicketPoint4, thicketPoint1}
 			patrolHunter2.patrolPositionTable = {thicketPoint2, thicketPoint5, thicketPoint4, thicketPoint1}
 
 			patrolHunter1 = Tanari:SpawnThicketPriest(thicketPoint2)
-			patrolHunter2 = Tanari:SpawnThicketTank(thicketPoint2+RandomVector(100))
+			patrolHunter2 = Tanari:SpawnThicketTank(thicketPoint2 + RandomVector(100))
 			patrolHunter1.patrolPositionTable = {thicketPoint3, thicketPoint4, thicketPoint1, thicketPoint2}
 			patrolHunter2.patrolPositionTable = {thicketPoint3, thicketPoint4, thicketPoint1, thicketPoint2}
 		end)
 
 		Timers:CreateTimer(3, function()
 			local patrolHunter1 = Tanari:SpawnHeadhunter(thicketPoint3)
-			local patrolHunter2 = Tanari:SpawnThicketTank(thicketPoint3+RandomVector(100))
+			local patrolHunter2 = Tanari:SpawnThicketTank(thicketPoint3 + RandomVector(100))
 			patrolHunter1.patrolPositionTable = {thicketPoint4, thicketPoint5, thicketPoint2, thicketPoint1, thicketPoint3}
 			patrolHunter2.patrolPositionTable = {thicketPoint4, thicketPoint5, thicketPoint2, thicketPoint1, thicketPoint3}
 		end)
 
 		local patrolHunter1 = Tanari:SpawnHeadhunter(thicketPoint4)
-		local patrolHunter2 = Tanari:SpawnThicketPriest(thicketPoint4+RandomVector(100))
-		local patrolHunter3 = Tanari:SpawnHeadhunter(thicketPoint4+RandomVector(100))
+		local patrolHunter2 = Tanari:SpawnThicketPriest(thicketPoint4 + RandomVector(100))
+		local patrolHunter3 = Tanari:SpawnHeadhunter(thicketPoint4 + RandomVector(100))
 		patrolHunter1.patrolPositionTable = {thicketPoint1, thicketPoint2, thicketPoint3, thicketPoint5, thicketPoint4}
 		patrolHunter2.patrolPositionTable = {thicketPoint1, thicketPoint2, thicketPoint3, thicketPoint5, thicketPoint4}
 		patrolHunter3.patrolPositionTable = {thicketPoint1, thicketPoint2, thicketPoint3, thicketPoint5, thicketPoint4}
 
 		patrolHunter1 = Tanari:SpawnHeadhunter(thicketPoint5)
-		patrolHunter2 = Tanari:SpawnThicketPriest(thicketPoint5+RandomVector(100))
-		patrolHunter3 = Tanari:SpawnThicketTank(thicketPoint5+RandomVector(100))
+		patrolHunter2 = Tanari:SpawnThicketPriest(thicketPoint5 + RandomVector(100))
+		patrolHunter3 = Tanari:SpawnThicketTank(thicketPoint5 + RandomVector(100))
 		patrolHunter1.patrolPositionTable = {thicketPoint6, thicketPoint1, thicketPoint3, thicketPoint4, thicketPoint5}
 		patrolHunter2.patrolPositionTable = {thicketPoint6, thicketPoint1, thicketPoint3, thicketPoint4, thicketPoint5}
 		patrolHunter3.patrolPositionTable = {thicketPoint6, thicketPoint1, thicketPoint3, thicketPoint4, thicketPoint5}
 
 		patrolHunter1 = Tanari:SpawnThicketPriest(thicketPoint6)
-		patrolHunter2 = Tanari:SpawnThicketPriest(thicketPoint6+RandomVector(100))
-		patrolHunter3 = Tanari:SpawnHeadhunter(thicketPoint6+RandomVector(100))
+		patrolHunter2 = Tanari:SpawnThicketPriest(thicketPoint6 + RandomVector(100))
+		patrolHunter3 = Tanari:SpawnHeadhunter(thicketPoint6 + RandomVector(100))
 		patrolHunter1.patrolPositionTable = {thicketPoint1, thicketPoint6}
 		patrolHunter2.patrolPositionTable = {thicketPoint1, thicketPoint6}
 		patrolHunter3.patrolPositionTable = {thicketPoint1, thicketPoint6}
 
 		Timers:CreateTimer(4, function()
 			patrolHunter1 = Tanari:SpawnHeadhunter(thicketPoint7)
-			patrolHunter2 = Tanari:SpawnThicketPriest(thicketPoint7+RandomVector(100))
-			patrolHunter3 = Tanari:SpawnThicketTank(thicketPoint7+RandomVector(100))
+			patrolHunter2 = Tanari:SpawnThicketPriest(thicketPoint7 + RandomVector(100))
+			patrolHunter3 = Tanari:SpawnThicketTank(thicketPoint7 + RandomVector(100))
 			patrolHunter1.patrolPositionTable = {thicketPoint1, thicketPoint2, thicketPoint5, thicketPoint4, thicketPoint5, thicketPoint7}
 			patrolHunter2.patrolPositionTable = {thicketPoint1, thicketPoint2, thicketPoint5, thicketPoint4, thicketPoint5, thicketPoint7}
 			patrolHunter3.patrolPositionTable = {thicketPoint1, thicketPoint2, thicketPoint5, thicketPoint4, thicketPoint5, thicketPoint7}
 		end)
 	end)
 	Timers:CreateTimer(20, function()
-		Tanari:SpawnHighPriestTorall(Vector(9664, 1408), Vector(-1,-1))
+		Tanari:SpawnHighPriestTorall(Vector(9664, 1408), Vector(-1, -1))
 		local hunter1 = Tanari:SpawnHeadhunter(Vector(9518, 1441))
 		hunter1.patrolPositionTable = {Vector(9326, 1400), Vector(9518, 1441)}
 		local hunter2 = Tanari:SpawnThicketPriest(Vector(9838, 1339))
@@ -76,29 +76,29 @@ function Tanari:SpawnThicket()
 	end)
 
 	Timers:CreateTimer(3, function()
-		Tanari:SpawnThicketWatcher(Vector(6080, 2368), Vector(1,-1))
-		Tanari:SpawnThicketWatcher(Vector(7326, 3199), Vector(-1,1))
-		Tanari:SpawnThicketWatcher(Vector(7300, 1101), Vector(0,1))
-		Tanari:SpawnThicketWatcher(Vector(9229, 1707), Vector(0,-1))
-		Tanari:SpawnThicketWatcher(Vector(9229, 3551), Vector(-0.2,1))
+		Tanari:SpawnThicketWatcher(Vector(6080, 2368), Vector(1, -1))
+		Tanari:SpawnThicketWatcher(Vector(7326, 3199), Vector(-1, 1))
+		Tanari:SpawnThicketWatcher(Vector(7300, 1101), Vector(0, 1))
+		Tanari:SpawnThicketWatcher(Vector(9229, 1707), Vector(0, -1))
+		Tanari:SpawnThicketWatcher(Vector(9229, 3551), Vector(-0.2, 1))
 		Timers:CreateTimer(4, function()
-			Tanari:SpawnThicketWatcher(Vector(9918, 4122), Vector(-1,1))
-			Tanari:SpawnThicketWatcher(Vector(8823, 4544), Vector(0,-1))
-			Tanari:SpawnThicketWatcher(Vector(6718, 4392), Vector(1,0))
+			Tanari:SpawnThicketWatcher(Vector(9918, 4122), Vector(-1, 1))
+			Tanari:SpawnThicketWatcher(Vector(8823, 4544), Vector(0, -1))
+			Tanari:SpawnThicketWatcher(Vector(6718, 4392), Vector(1, 0))
 			if GameState:GetDifficultyFactor() > 1 then
-				Tanari:SpawnThicketWatcher(Vector(6080, 3199), Vector(1,0))
-				Tanari:SpawnThicketWatcher(Vector(7086, 2651), Vector(-1,0))
-				Tanari:SpawnThicketWatcher(Vector(7834, 881), Vector(0.4,1))
-				Tanari:SpawnThicketWatcher(Vector(8441, 4739), Vector(0,1))
-				Tanari:SpawnThicketWatcher(Vector(7977, 4393), Vector(-1,0))
-				Tanari:SpawnThicketWatcher(Vector(6718, 3896), Vector(0,-1))
+				Tanari:SpawnThicketWatcher(Vector(6080, 3199), Vector(1, 0))
+				Tanari:SpawnThicketWatcher(Vector(7086, 2651), Vector(-1, 0))
+				Tanari:SpawnThicketWatcher(Vector(7834, 881), Vector(0.4, 1))
+				Tanari:SpawnThicketWatcher(Vector(8441, 4739), Vector(0, 1))
+				Tanari:SpawnThicketWatcher(Vector(7977, 4393), Vector(-1, 0))
+				Tanari:SpawnThicketWatcher(Vector(6718, 3896), Vector(0, -1))
 			end
 		end)
 	end)
 end
 
 function Tanari:SpawnHeadhunter(position)
-	local hunter = Tanari:SpawnDungeonUnit("tanari_headhunter", position, 2, 4, "huskar_husk_anger_0"..RandomInt(1,6), Vector(1,0), false)
+	local hunter = Tanari:SpawnDungeonUnit("tanari_headhunter", position, 2, 4, "huskar_husk_anger_0"..RandomInt(1, 6), Vector(1, 0), false)
 	Events:AdjustBossPower(hunter, 2, 2, false)
 	hunter.itemLevel = 26
 	hunter.patrolSlow = 40
@@ -109,7 +109,7 @@ function Tanari:SpawnHeadhunter(position)
 end
 
 function Tanari:SpawnThicketPriest(position)
-	local hunter = Tanari:SpawnDungeonUnit("tanari_thicket_priest", position, 2, 4, "meepo_meepo_anger_0"..RandomInt(1,3), Vector(1,0), false)
+	local hunter = Tanari:SpawnDungeonUnit("tanari_thicket_priest", position, 2, 4, "meepo_meepo_anger_0"..RandomInt(1, 3), Vector(1, 0), false)
 	Events:AdjustBossPower(hunter, 2, 2, false)
 	hunter.itemLevel = 26
 	hunter.patrolSlow = 40
@@ -120,7 +120,7 @@ function Tanari:SpawnThicketPriest(position)
 end
 
 function Tanari:SpawnThicketTank(position)
-	local hunter = Tanari:SpawnDungeonUnit("tanari_thicket_pain_absorber", position, 2, 4, "elder_titan_elder_anger_04", Vector(1,0), false)
+	local hunter = Tanari:SpawnDungeonUnit("tanari_thicket_pain_absorber", position, 2, 4, "elder_titan_elder_anger_04", Vector(1, 0), false)
 	Events:AdjustBossPower(hunter, 2, 2, false)
 	hunter.itemLevel = 26
 	hunter.patrolSlow = 40
@@ -130,19 +130,17 @@ function Tanari:SpawnThicketTank(position)
 	return hunter
 end
 
-
-
 function Tanari:SpawnPrimitiveAmbusher(position, fv)
-	local hunter = Tanari:SpawnDungeonUnit("tanari_primitive_hunter", position, 0, 1, "troll_warlord_troll_anger_0"..RandomInt(1,6), fv, true)
+	local hunter = Tanari:SpawnDungeonUnit("tanari_primitive_hunter", position, 0, 1, "troll_warlord_troll_anger_0"..RandomInt(1, 6), fv, true)
 	hunter.itemLevel = 18
 	WallPhysics:Jump(hunter, fv, 24, 22, 20, 1.2)
-	StartAnimation(hunter, {duration=1, activity=ACT_DOTA_RUN, rate=1, translate="haste"})
+	StartAnimation(hunter, {duration = 1, activity = ACT_DOTA_RUN, rate = 1, translate = "haste"})
 	hunter.dominion = true
 	return hunter
 end
 
 function Tanari:SpawnHighPriestTorall(position, fv)
-	local hunter = Tanari:SpawnDungeonUnit("tanari_thicket_high_priest", position, 3, 6, "meepo_meepo_anger_0"..RandomInt(1,3), fv, false)
+	local hunter = Tanari:SpawnDungeonUnit("tanari_thicket_high_priest", position, 3, 6, "meepo_meepo_anger_0"..RandomInt(1, 3), fv, false)
 	Events:AdjustBossPower(hunter, 3, 3, false)
 	hunter.itemLevel = 32
 	hunter.dominion = true
@@ -151,17 +149,17 @@ end
 function Tanari:SpawnSideThicket()
 	local basePos = Vector(5120, 6464)
 	for i = 1, 20, 1 do
-		Timers:CreateTimer(i*0.4, function()
-			Tanari:SpawnSapling(basePos+Vector(0,0,50*i)+RandomVector(120), RandomVector(1))
+		Timers:CreateTimer(i * 0.4, function()
+			Tanari:SpawnSapling(basePos + Vector(0, 0, 50 * i) + RandomVector(120), RandomVector(1))
 		end)
 	end
 	for j = 1, 12, 1 do
-		Timers:CreateTimer(j*0.7, function()
+		Timers:CreateTimer(j * 0.7, function()
 			Tanari:SpawnThicketBat(Vector(5120, 8576))
 		end)
 	end
-	Tanari:SpawnWildTroll(Vector(4928, 7616), Vector(-1,-1))
-	Tanari:SpawnThicketBatMatriarch(Vector(5222, 9877), Vector(0,-1))
+	Tanari:SpawnWildTroll(Vector(4928, 7616), Vector(-1, -1))
+	Tanari:SpawnThicketBatMatriarch(Vector(5222, 9877), Vector(0, -1))
 	-- local item = RPCItems:CreateUnstashable("item_rpc_thicket_bat_egg", "rare", "Thicket Bat Egg", -1, false, "Tanari Jungle only", "thicket_bat_egg_description")
 	-- CreateItemOnPositionSync(Vector(5120, 10162), item)
 	-- local particleName = RPCItems:GetRarityParticle("rare")
@@ -174,7 +172,7 @@ function Tanari:SpawnSapling(position, fv)
 	sapling.itemLevel = 27
 	sapling:RemoveAbility("knockback_immunity")
 	sapling:RemoveAbility("desert_ruins_ability")
-	sapling:SetModelScale(0.6)	
+	sapling:SetModelScale(0.6)
 	sapling.dominion = true
 	CustomAbilities:QuickAttachParticle("particles/econ/items/pets/pet_frondillo/pet_spawn_dirt_frondillo.vpcf", sapling, 1)
 end
@@ -187,7 +185,7 @@ function Tanari:SpawnWildTroll(position, fv)
 end
 
 function Tanari:SpawnThicketBat(position)
-	local sapling = Tanari:SpawnDungeonUnit("tanari_thicket_bat", position, 0, 2, "Tanari.Bat.Aggro", Vector(1,0), false)
+	local sapling = Tanari:SpawnDungeonUnit("tanari_thicket_bat", position, 0, 2, "Tanari.Bat.Aggro", Vector(1, 0), false)
 	sapling.itemLevel = 32
 	sapling.dominion = true
 end
@@ -217,7 +215,7 @@ function Tanari:SpawnThicketWatcher(position, fv)
 	sapling:AddNoDraw()
 	sapling.dominion = true
 
-	local luck = RandomInt(1,6-GameState:GetDifficultyFactor())
+	local luck = RandomInt(1, 6 - GameState:GetDifficultyFactor())
 	if luck == 1 then
 		local sapling = Tanari:SpawnDungeonUnit("thicket_watcher", position, 1, 2, nil, fv, false)
 		sapling.itemLevel = 26
