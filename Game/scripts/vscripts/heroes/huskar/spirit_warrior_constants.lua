f56fb31ca00ee4dc04905dabfe1d85b769470c38
redledcat <danmarch@mail.ru>
2019-07-06 14:23:01 +0300

    spirit warrior w2

--- Game/scripts/vscripts/heroes/huskar/spirit_warrior_constants.lua
+++ Game/scripts/vscripts/heroes/huskar/spirit_warrior_constants.lua
@@ -2,6 +2,7 @@ SPIRIT_WARRIOR_Q1_BASE = 220
 SPIRIT_WARRIOR_Q1_DMG = 285
 
 SPIRIT_WARRIOR_W1_BASE_DMG_PER_MANA = 0.08
+SPIRIT_WARRIOR_W2_BASE_DMG = 640
 
 spirit_warrior_q3_dmg_pct = 0.1
 spirit_warrior_arcana_w4 = 0.08
