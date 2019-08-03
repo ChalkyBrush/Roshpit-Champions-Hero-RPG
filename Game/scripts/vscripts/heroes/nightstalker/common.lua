fe536a9740b4b2263f2d4f2f0beee5180a443f31
Sergej Demchenko <rutelun@ya.ru>
2019-08-04 00:51:59 +0300

    Fix T1 glyph overwrited e2 radius

--- Game/scripts/vscripts/heroes/nightstalker/common.lua
+++ Game/scripts/vscripts/heroes/nightstalker/common.lua
@@ -4,6 +4,9 @@ local shadowsModifiers = {
     aura = 'modifier_chernobog_shadows_aura',
     enemy_effect = 'modifier_chernobog_shadows_enemy_effect'
 }
+for modifierPath, modifier in pairs(shadowsModifiers) do
+    LinkLuaModifier(modifier, "heroes/nightstalker/modifiers/t1_shadows_"..modifierPath, LUA_MODIFIER_MOTION_NONE)
+end
 function chenobog_make_right_cooldown(caster, ability, abilityLetter)
     local cooldownAmplify = 1
     if abilityLetter == 'q' then
@@ -19,15 +22,16 @@ function init_shadows_values_for_ability(data)
     local radius = data.radius
     local thinkInterval = data.thinkInterval
     local damagePercent = data.damagePercent
+    local prefix = data.prefix or ''
 
 
     if data.ability:GetCaster():HasModifier('modifier_chernobog_glyph_2_1') then
         radius = radius * CHERNOBOG_T21_RADIUS_AMP
     end
 
-    data.ability.shadowsAuraRadius = radius
-    data.ability.shadowsThinkInterval = thinkInterval
-    data.ability.shadowsDamagePercent = damagePercent
+    data.ability[prefix .. 'shadowsAuraRadius'] = radius
+    data.ability[prefix .. 'shadowsThinkInterval'] = thinkInterval
+    data.ability[prefix .. 'shadowsDamagePercent'] = damagePercent
 end
 
 function hasArcana2(caster)
@@ -56,7 +60,8 @@ function onBlink(caster,ability, startPoint, endPoint)
                 ability = ability,
                 radius = radius,
                 damagePercent = caster.e4_level * CHERNOBOG_ARCANA2_E4_DMG_PCT,
-                thinkInterval = CHERNOBOG_ARCANA2_E4_INTERVAL
+                thinkInterval = CHERNOBOG_ARCANA2_E4_INTERVAL,
+                prefix = 't1_',
             })
         else
             canCreateAura = false
@@ -68,11 +73,13 @@ function onBlink(caster,ability, startPoint, endPoint)
                 radius = radius,
                 damagePercent = caster.e2_level * CHERNOBOG_E2_DMG_PCT,
                 thinkInterval = CHERNOBOG_E2_INTERVAL / (1 + caster.e4_level * CHERNOBOG_E4_SHADOWS_INTERVAL_SCALE),
+                prefix = 't1_',
             })
         else
             canCreateAura = false
         end
     end
+    print('blink radius was' .. radius)
     if canCreateAura then
         Util.Ability:MakeThinker(caster, ability, shadowsModifiers.aura, startPoint, getShadowsDuration(caster, CHERNOBOG_T11_DURATION))
         Util.Ability:MakeThinker(caster, ability, shadowsModifiers.aura, endPoint, getShadowsDuration(caster, CHERNOBOG_T11_DURATION))
