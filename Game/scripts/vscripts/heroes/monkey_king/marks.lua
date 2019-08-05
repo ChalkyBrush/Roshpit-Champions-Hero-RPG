f5ba47dc3ba560cb8868e445d1caf7752e04d190
Sergej Demchenko <rutelun@ya.ru>
2019-08-05 22:53:28 +0300

    T4 glyph now gives 7 shields instead of 4, Q3 now passive

--- Game/scripts/vscripts/heroes/monkey_king/marks.lua
+++ Game/scripts/vscripts/heroes/monkey_king/marks.lua
@@ -20,6 +20,32 @@ function draghor_main_think(event)
 			crowAbility:SetActivated(false)
 		end
 	end
+	if caster.q3_level > 0 then
+		if caster:IsRooted()
+			or caster:IsStunned()
+			or caster:IsFrozen()
+			or caster:IsFakeStunned()
+			then
+			return
+		end
+		local modifiers = caster:FindAllModifiers()
+		for i = 1, #modifiers, 1 do
+			local modifier = modifiers[i]
+			local modifierMaker = modifier:GetCaster()
+			if not modifier.djanghorQ3Increase then
+				modifier.djanghorQ3Increase = true
+				if modifierMaker then
+					if (modifierMaker:GetEntityIndex() == caster:GetEntityIndex() or modifierMaker:GetEntityIndex() == caster.InventoryUnit:GetEntityIndex()) then
+						local durationRemaining = modifier:GetRemainingTime()
+						if durationRemaining > 0 then
+							local durationIncrease = 0.3 + DJANGHOR_Q3_BUFF_DURATION_INCREASE * caster.q3_level
+							modifier:SetDuration(durationRemaining + durationIncrease, true)
+						end
+					end
+				end
+			end
+		end
+	end
 end
 
 function mark_of_the_fang(event)
