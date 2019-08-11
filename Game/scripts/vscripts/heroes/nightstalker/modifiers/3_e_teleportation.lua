28e9c6b62de4bb5c69411587f113208259fcd996
Sergej Demchenko <rutelun@ya.ru>
2019-08-12 00:23:13 +0300

    Fix chernobog e3

--- Game/scripts/vscripts/heroes/nightstalker/modifiers/3_e_teleportation.lua
+++ Game/scripts/vscripts/heroes/nightstalker/modifiers/3_e_teleportation.lua
@@ -127,8 +127,8 @@ end
 function class:OnOrderMove(data)
     local parent = self:GetParent()
     local newPosition = GetGroundPosition(Vector(data.position_x, data.position_y, 0), parent)
-    local distance = WallPhysics:GetDistance2d(parentOrigin, newPosition)
     local parentOrigin = parent:GetAbsOrigin()
+    local distance = WallPhysics:GetDistance2d(parentOrigin, newPosition)
     local maxDistance = self:GetMaxDistance()
     if distance > maxDistance then
         local clampedVector = WallPhysics:ClampedVector(parentOrigin, newPosition, maxDistance)
