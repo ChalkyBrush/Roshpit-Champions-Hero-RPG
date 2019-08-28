23d48aa551d7f9adca479e69fcadd314e5d59801
GoToNightmare <holdscroll@gmail.com>
2019-08-28 11:26:53 +0500

    Update general.lua

--- Game/scripts/vscripts/npc_abilities/general.lua
+++ Game/scripts/vscripts/npc_abilities/general.lua
@@ -681,7 +681,7 @@ function ms_thinker(event)
 	if modifier_emerald_speed_runners then
 		local msValue = modifier_emerald_speed_runners:GetAbility():GetSpecialValueFor("property_one")
 		--print("modifier_emerald_speed_runners "..tostring(msValue))
-		max_ms = msValue
+		max_ms = math.max(msValue, max_ms)
 		actual_movespeed = math.max(msValue, actual_movespeed)
 	end
 	
