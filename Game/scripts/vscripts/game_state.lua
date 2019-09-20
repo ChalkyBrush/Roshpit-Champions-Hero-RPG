34566333cd8f9a9270b02628b48ace09a62b71ed
redledcat <danmarch@mail.ru>
2019-09-20 14:50:47 +0300

    arena mithril constants (and redfall gamestate line)

--- Game/scripts/vscripts/game_state.lua
+++ Game/scripts/vscripts/game_state.lua
@@ -28,8 +28,10 @@ require('/items/constants/helm')
 require('/items/constants/trinket')
 
 require('/worlds/winterblight/constants/enemies_abilities')
+require('/worlds/redfall/constants/mithril')
 require('/worlds/winterblight/constants/mithril')
 require('/worlds/tanari/constants/mithril')
+require('/worlds/arena/constants/mithril')
 
 local heroes = {
 	venomort = require('/heroes/hero_necrolyte/scales')}
