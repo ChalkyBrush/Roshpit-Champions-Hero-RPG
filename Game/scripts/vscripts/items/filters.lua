0a6818e420d489090538a94a93467db35087c8e2
redledcat <danmarch@mail.ru>
2019-09-23 22:53:03 +0300

    staggering knight

--- Game/scripts/vscripts/items/filters.lua
+++ Game/scripts/vscripts/items/filters.lua
@@ -522,11 +522,11 @@ end
 function Filters:ApplyStun(caster, duration, target)
     local mult = 1
     if caster:HasModifier("modifier_knight_crusher_armor") then
-        mult = mult + 0.5
+        mult = mult + STAGGERING_KNIGHT_CRUSHER_STUN_DURATION_INCREASE/100
     end
 
     if target:IsHero() then
-        mult = mult * 0.5
+        mult = mult * (100-STAGGERING_KNIGHT_CRUSHER_STUN_IMMUNITY_COUNTER_DECREASE)/100
     end
     if caster:HasModifier("modifier_steelforge_passive") then
         caster.w_2_level = caster:GetRuneValue("w", 2)
