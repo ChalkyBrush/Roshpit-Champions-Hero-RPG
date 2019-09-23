3f0cff1500294e14ee95742d1de7c16832d66042
redledcat <danmarch@mail.ru>
2019-09-23 19:41:38 +0300

    seraphic

--- Game/scripts/vscripts/items/special_item_effects.lua
+++ Game/scripts/vscripts/items/special_item_effects.lua
@@ -4457,7 +4457,7 @@ function seraphic_soul_hit(event)
 	local abilityLevel = hero:GetAbilityByIndex(DOTA_W_SLOT):GetLevel()
 	if target:IsAlive() then
 		EmitSoundOn("RPCItem.SoulVestImpact", target)
-		local damage = OverflowProtectedGetAverageTrueAttackDamage(hero) * 0.5 * abilityLevel
+		local damage = OverflowProtectedGetAverageTrueAttackDamage(hero) * SERAPHIC_SOULVEST_ATTACK_TO_DMG/100 * abilityLevel
 		Filters:ApplyItemDamage(target, hero, damage, DAMAGE_TYPE_PURE, ability, RPC_ELEMENT_HOLY, RPC_ELEMENT_NONE)
 	end
 end
