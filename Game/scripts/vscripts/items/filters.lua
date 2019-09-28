32cbc0ddb901169cf2219005ac880b90c4da20a3
Nintai <nintai@hotmail.de>
2019-09-28 15:27:20 +0200

    Redfall Runners:Now increases movement speed and movement speed cap by 5 per % of missing health.2nd Slot now rolls T1/T2 runes with a max of 91

--- Game/scripts/vscripts/items/filters.lua
+++ Game/scripts/vscripts/items/filters.lua
@@ -1119,10 +1119,6 @@ function Filters:ApplyEskills(caster)
     if caster:HasModifier("modifier_moon_techs") then
         Filters:MoonTechRunners(caster)
     end
-    if caster:HasModifier("modifier_redfall_runners") then
-        caster:RemoveModifierByName("modifier_redfall_runners_hidden_buff")
-        caster.foot:ApplyDataDrivenModifier(caster.InventoryUnit, caster, "modifier_redfall_runners_buff", {duration = REDFALL_RUNNERS_DURATION})
-    end
     if caster:HasModifier("modifier_guard_of_feronia") then
         caster.body:ApplyDataDrivenModifier(caster.InventoryUnit, caster, "modifier_guard_of_feronia_shield", {duration = GUARD_OF_FERONIA_SHIELD_DURATION_E})
     end
@@ -1588,11 +1584,6 @@ function Filters:TakeArgumentsAndApplyDamage(victim, attacker, damage, damage_ty
             if attacker:GetUnitName() == "npc_dota_hero_spirit_breaker" then
                 Filters:ApplyStun(attacker, DUSKBRINGER_IMMORTAL_WEAPON_2_W_STUN, victim)
             end
-        end
-        if attacker:HasModifier("modifier_redfall_runners_buff") or attacker:HasModifier("modifier_redfall_runners_hidden_buff") then
-            attacker:RemoveModifierByName("modifier_redfall_runners_buff")
-            attacker.foot:ApplyDataDrivenModifier(attacker.InventoryUnit, attacker, "modifier_redfall_runners_hidden_buff", {duration = 0.2})
-            damageMult = damageMult + REDFALL_RUNNERS_BAD_MULT/100
         end
         if attacker:HasModifier("modifier_trickster_mask") then
             local randomFactor = RandomInt(TRICKSTER_MASK_W_DMG_MULT_MIN * 10, TRICKSTER_MASK_W_DMG_MULT_MAX * 10) / 10
