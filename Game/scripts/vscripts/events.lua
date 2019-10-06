31caef2359c3c9f7f72af8c9c7bcd0a0030d23a0
GoToNightmare <holdscroll@gmail.com>
2019-10-06 08:47:33 +0500

    -unstuck command

--- Game/scripts/vscripts/events.lua
+++ Game/scripts/vscripts/events.lua
@@ -443,6 +443,15 @@ function GameMode:OnPlayerChat(keys)
 	-- --print("boom")
 	-- PlayerResource:GetPlayer(keys.playerid):GetAssignedHero():HasModifier(nil)
 	-- end
+	if string.match(text, "-suicide") or string.match(text, "-unstuck") then
+		local playerHero = PlayerResource:GetPlayer(keys.playerid):GetAssignedHero()
+		local modifier_recently_respawned = playerHero:HasModifier("modifier_recently_respawned")
+		if modifier_recently_respawned or not playerHero:IsAlive() then
+			print("! dead !")
+		else
+			playerHero:ForceKill(true)
+		end
+	end
 	if string.match(text, "dbg") then
 		-- Serengaard:KillAllNeutrals()
 		-- local position = PlayerResource:GetPlayer(keys.playerid):GetAssignedHero():GetAbsOrigin()
