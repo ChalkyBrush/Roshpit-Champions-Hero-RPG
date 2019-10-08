c5b7726947f9922a07e4a3cc04fcc9c95266d46d
redledcat <danmarch@mail.ru>
2019-10-08 21:29:00 +0300

    epsilons eyeglass considers low range distance as 700

--- Game/scripts/vscripts/items/special_item_effects.lua
+++ Game/scripts/vscripts/items/special_item_effects.lua
@@ -4942,6 +4942,7 @@ function eyeglass_attack(event)
 		return false
 	end
 	local distance = math.min(WallPhysics:GetDistance(attacker:GetAbsOrigin(), target:GetAbsOrigin()), EPSILON_EYEGLASS_MAX_RANGE_FOR_DAMAGE)
+	distance = math.max(distance, EPSILON_EYEGLASS_MIN_RANGE)
 	local damage = EPSILON_EYEGLASS_DAMAGE_BASE * attacker:GetLevel() * distance ^ 3
 
 	Filters:ApplyItemDamage(target, attacker, damage, DAMAGE_TYPE_PHYSICAL, event.ability, RPC_ELEMENT_HOLY, RPC_ELEMENT_COSMOS)
