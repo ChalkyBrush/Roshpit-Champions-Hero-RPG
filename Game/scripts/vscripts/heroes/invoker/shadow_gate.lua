19eea7f1fe9555f9df364e3183d8bf4cb30ad9ba
Ryan Racioppo <ryanracioppo@gmail.com>
2019-10-27 15:18:32 -0400

    fix conj

--- Game/scripts/vscripts/heroes/invoker/shadow_gate.lua
+++ Game/scripts/vscripts/heroes/invoker/shadow_gate.lua
@@ -4,7 +4,7 @@ function shadow_gate_cast(event)
 	local target = event.target_points[1]
 	local casterOrigin = caster:GetAbsOrigin()
 	target = WallPhysics:WallSearch(casterOrigin, target, caster)
-	local b_c_level = get_b_c_level(caster)
+	local b_c_level = caster:GetRuneValue("e", 2)
 
 	ability.e_4_level = Runes:GetTotalRuneLevel(caster, 4, "e_4", "conjuror")
 	local lanceParticle = "particles/units/heroes/hero_phantom_lancer/shadow_flare_phantomlancer_spiritlance_projectile.vpcf"
