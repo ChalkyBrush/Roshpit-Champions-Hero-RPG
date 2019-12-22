256e54a807906cc28a3a57296ea90d0a78be710e
Ryan Racioppo <ryanracioppo@gmail.com>
2019-12-22 10:20:38 -0500

    remove lethal check

--- Game/scripts/vscripts/game_state.lua
+++ Game/scripts/vscripts/game_state.lua
@@ -3379,7 +3379,6 @@ function GameState:FilterDamage(filterTable)
 	end
 
 	--LETHAL CHECK
-	filterTable["damage"] = victim:GetHealth()*100
 	if filterTable["damage"] >= victim:GetHealth() then
 		local rezzed = false
 		if victim:HasModifier("modifier_phoenix_emblem") then
