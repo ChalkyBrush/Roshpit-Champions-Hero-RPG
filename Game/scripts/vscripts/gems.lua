464f23984436a46a2e28009ebb144ff6ae5c86ca
Ryan Racioppo <ryanracioppo@gmail.com>
2019-12-29 13:24:03 -0500

    fix

--- Game/scripts/vscripts/gems.lua
+++ Game/scripts/vscripts/gems.lua
@@ -68,7 +68,7 @@ function Gems:GetRewardAndSpawnPositionByEventName(event_name)
 		spawn_args["position"] = Vector(-15040, -3904)
 		spawn_args["fv"] = Vector(1,0)
 		spawn_args["reward"] = 9		
-	elseif event_name == "water_temple_spirit_boss" then
+	elseif event_name == "tanari_water_spirit_boss" then
 		spawn_args["position"] = Vector(14400, -2432)
 		spawn_args["fv"] = Vector(-1,1)
 		spawn_args["reward"] = 9	
