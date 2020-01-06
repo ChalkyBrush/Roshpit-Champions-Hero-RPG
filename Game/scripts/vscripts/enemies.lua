ae117645aa2f7b6e0efed8db6471e1b59391f315
Ryan Racioppo <ryanracioppo@gmail.com>
2020-01-06 17:39:25 -0500

    paragon exceptions

--- Game/scripts/vscripts/enemies.lua
+++ Game/scripts/vscripts/enemies.lua
@@ -411,7 +411,7 @@ function CDOTA_BaseNPC:IsRegularEnemy(compare_unit)
 	end
 end
 
-Enemies.PARAGON_EXCEPTION_TABLE = {"pixie_minion", "npc_dummy_unit", "winterblight_zefnar", "npc_flying_dummy_vision", "water_temple_tentacle_switch"}
+Enemies.PARAGON_EXCEPTION_TABLE = {"pixie_minion", "npc_dummy_unit", "winterblight_zefnar", "npc_flying_dummy_vision", "water_temple_tentacle_switch", "ekkan_corpse", "tanari_wind_spark_dummy"}
 
 function Enemies:ParagonChance(unit)
 	if WallPhysics:DoesTableHaveValue(Enemies.PARAGON_EXCEPTION_TABLE, unit:GetUnitName()) then
