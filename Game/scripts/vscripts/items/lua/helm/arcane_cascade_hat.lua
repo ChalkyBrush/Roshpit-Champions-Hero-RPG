770bbe3b409d7d6233fb4f8ed2a4e23cd3fbc97b
Maurdekhai <maurdekhai@gmail.com>
2020-02-07 00:12:12 +0300

    arcane cascade hat fix

--- Game/scripts/vscripts/items/lua/helm/arcane_cascade_hat.lua
+++ Game/scripts/vscripts/items/lua/helm/arcane_cascade_hat.lua
@@ -18,20 +18,25 @@ LinkLuaModifier(debuffModifierName, "items/lua/helm/arcane_cascade_hat", LUA_MOD
 function itemClass:GetClassName()
     return itemClassName
 end
+
 function itemClass:GetName()
     return 'Arcane Cascade Hat'
 end
+
 function itemClass:GetModifierName()
     return modifierName
 end
+
 function itemClass:HasRuneSlots()
     return true
 end
+
 function itemClass:RollProperty1(maxFactor)
     self.newItemTable.property1 = 1
     self.newItemTable.property1name = "!immortal!_modifier_arcane_cascade_hat"
     self:SetSpecialValue("arcane_cascade_hat", "#E558F5")
 end
+
 function itemClass:RollProperty2(item_level)
     RPCItems:RollBasicItemProperty(self, self:GetSlotNumber(), 2, item_level, "max_mana", 1.5)
 end
@@ -39,23 +44,25 @@ end
 function itemClass:RollProperty3(item_level)
     RPCItems:RollBasicItemProperty(self, self:GetSlotNumber(), 3, item_level, "intelligence", 1.5)
 end
+
 function itemClass:RollArmor(item_level)
     RPCItems:GrantItemBaseArmor(self, item_level, 0)
 end
+
 function itemClass:RollMagicArmor(item_level)
     RPCItems:GrantItemBaseMagicArmor(self, item_level, 3)
 end
+
 function modifierClass:OnCreated()
-    if not IsServer() then
-        return
-    end
+    if not IsServer() then return end
+    
     self:StartIntervalThink(ARCANE_CASCADE_TICKRATE)
 end
+
 function modifierClass:OnIntervalThink()
-    if not IsServer() then
-        return
-    end
-    local caster = self:GetCaster()
+    if not IsServer() then return end
+
+    local caster = self:GetParent()
     local manaDrain = caster:GetMaxMana() * (ARCANE_CASCADE_MANA_DRAIN + self:GetAbility():GetFinalGemPropertyValue("sapphire", ARCANE_CASCADE_SAPPHIRE)/100)
     if manaDrain > caster:GetMana() then
         manaDrain = caster:GetMana()
@@ -63,33 +70,43 @@ function modifierClass:OnIntervalThink()
     self:GetAbility().damage = manaDrain * ARCANE_CASCADE_DAMAGE
     caster:ReduceMana(manaDrain)
 end
+
 function modifierClass:IsHidden()
     return true
 end
+
 function modifierClass:IsBuff()
     return true
 end
+
 function modifierClass:IsAura()
     return true
 end
+
 function modifierClass:IsAuraActiveOnDeath()
     return false
 end
+
 function modifierClass:GetAuraRadius()
     return ARCANE_CASCADE_RANGE
 end
+
 function modifierClass:GetAuraSearchTeam()
     return DOTA_UNIT_TARGET_TEAM_ENEMY
 end
+
 function modifierClass:GetAuraSearchType()
     return (DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC)
 end
+
 function modifierClass:GetAuraSearchFlags()
     return DOTA_UNIT_TARGET_FLAG_NONE
 end
+
 function modifierClass:RemoveOnDeath()
     return false
 end
+
 function modifierClass:GetModifierAura()
     return debuffModifierName
 end
@@ -98,40 +115,45 @@ end
 --ENEMY DEBUFF--
 ----------------
 function debuffModifierClass:OnCreated()
-    if not IsServer() then
-        return
-    end
+    if not IsServer() then return end
+
     self:SetSpecialTypes({ MODIFIER_ROSHPIT_MAGIC_ARMOR_BONUS })
     self:StartIntervalThink(ARCANE_CASCADE_TICKRATE)
 end
 
 function debuffModifierClass:OnIntervalThink()
-    if not IsServer() then
-        return
-    end
+    if not IsServer() then return end
+
+    local caster = self:GetCaster()
     local target = self:GetParent()
     local ability = self:GetAbility()
     if ability.damage then
         --print("Damage: "..ability.damage)
 		local damage = ability.damage
-		Filters:ApplyItemDamage(target, self:GetCaster(), damage, DAMAGE_TYPE_MAGICAL, ability, RPC_ELEMENT_ARCANE, RPC_ELEMENT_NONE)
+		Filters:ApplyItemDamage(target, caster.hero, damage, DAMAGE_TYPE_MAGICAL, ability, RPC_ELEMENT_ARCANE, RPC_ELEMENT_NONE)
 	end
 end
+
 function debuffModifierClass:GetRoshpitMagicArmorBonus()
     return self:GetAbility():GetFinalGemPropertyValue("amethyst", ARCANE_CASCADE_AMETHYST)
 end
+
 function debuffModifierClass:IsHidden()
     return true
 end
+
 function debuffModifierClass:IsDebuff()
     return true
 end
+
 function debuffModifierClass:RemoveOnDeath()
     return true
 end
+
 function debuffModifierClass:GetEffectName()
     return "particles/items2_fx/arcane_cascade.vpcf"
 end
+
 function debuffModifierClass:GetEffectAttachType()
     return PATTACH_CUSTOMORIGIN_FOLLOW
 end
