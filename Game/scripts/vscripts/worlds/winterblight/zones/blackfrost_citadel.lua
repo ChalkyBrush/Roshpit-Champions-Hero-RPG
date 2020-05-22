d10ff3755a6bf0c7a3c473d08ec59abc6bf40cf6
Ryan Racioppo <ryanracioppo@gmail.com>
2020-05-22 12:15:46 -0400

    fix tower fate

--- Game/scripts/vscripts/worlds/winterblight/zones/blackfrost_citadel.lua
+++ Game/scripts/vscripts/worlds/winterblight/zones/blackfrost_citadel.lua
@@ -305,7 +305,7 @@ function Winterblight:SetupCastleData()
 		Winterblight.CASTLE_DATA["tarot"][17]["rooms"][1] = {index = 8, variant = 1}
 		Winterblight.CASTLE_DATA["tarot"][17]["rooms"][2] = {index = 9, variant = 1}
 		Winterblight.CASTLE_DATA["tarot"][17]["rooms"][3] = {index = 7, variant = 1}
-		Winterblight.CASTLE_DATA["tarot"][17]["rooms"][1] = {index = 12, variant = 1}
+		Winterblight.CASTLE_DATA["tarot"][17]["rooms"][4] = {index = 12, variant = 1}
 		Winterblight.CASTLE_DATA["tarot"][17]["rooms"][5] = {index = 2, variant = 1}
 		Winterblight.CASTLE_DATA["tarot"][17]["rooms"][6] = {index = 1, variant = 1}
 		Winterblight.CASTLE_DATA["tarot"][17]["rooms"][7] = {index = 10, variant = 1}
@@ -321,7 +321,7 @@ function Winterblight:SetupCastleData()
 		Winterblight.CASTLE_DATA["tarot"][18]["rooms"][1] = {index = 3, variant = 1}
 		Winterblight.CASTLE_DATA["tarot"][18]["rooms"][2] = {index = 6, variant = 1}
 		Winterblight.CASTLE_DATA["tarot"][18]["rooms"][3] = {index = 12, variant = 1}
-		Winterblight.CASTLE_DATA["tarot"][18]["rooms"][1] = {index = 10, variant = 1}
+		Winterblight.CASTLE_DATA["tarot"][18]["rooms"][4] = {index = 10, variant = 1}
 		Winterblight.CASTLE_DATA["tarot"][18]["rooms"][5] = {index = 9, variant = 1}
 		Winterblight.CASTLE_DATA["tarot"][18]["rooms"][6] = {index = 11, variant = 1}
 		Winterblight.CASTLE_DATA["tarot"][18]["rooms"][7] = {index = 1, variant = 1}
