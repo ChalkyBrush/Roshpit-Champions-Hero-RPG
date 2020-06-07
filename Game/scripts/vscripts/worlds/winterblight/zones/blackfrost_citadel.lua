9a075af77400630bdd823cadb730c23d51718304
redledcat <danmarch@mail.ru>
2020-06-07 20:42:07 +0300

    fixxx

--- Game/scripts/vscripts/worlds/winterblight/zones/blackfrost_citadel.lua
+++ Game/scripts/vscripts/worlds/winterblight/zones/blackfrost_citadel.lua
@@ -2754,11 +2754,12 @@ function Winterblight:CastleBossDeath(boss)
 	end
 	Timers:CreateTimer(6, function()
 		local vessel_luck = RandomInt(1, 5)
-		local vessel_bonus = 0
-			if vessel_luck = 1 then
-			vessel_bonus = 1
-			elseif vessel_luck = 2 then
-			vessel_bonus = 2
+			if vessel_luck < 2 then
+				vessel_bonus = 1
+			elseif vessel_luck < 3 then
+				vessel_bonus = 2
+			elseif vessel_luck <= 5 then
+				vessel_bonus = 0
 			end
 		for j = 1, Winterblight.Stones + vessel_bonus, 1 do
 			Timers:CreateTimer(j, function()
