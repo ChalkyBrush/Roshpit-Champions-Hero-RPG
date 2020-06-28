b576751ef9ffd7d00e18a02158c8cdca98769eb0
Ryan Racioppo <ryanracioppo@gmail.com>
2020-06-28 19:50:10 -0400

    setup weapon

---
+++ Game/scripts/vscripts/heroes/vengeful_spirit/weapon_scripts/require.lua
@@ -0,0 +1,3 @@
+require('heroes/vengeful_spirit/weapon_scripts/solunia_immortal_weapon_1')
+require('heroes/vengeful_spirit/weapon_scripts/solunia_immortal_weapon_2')
+require('heroes/vengeful_spirit/weapon_scripts/solunia_immortal_weapon_3')
