569c3c48c8d71cd68da4a903dc15bfcefdbd10a3
redledcat <danmarch@mail.ru>
2020-07-12 15:03:07 +0300

    axe weapon 3 fix

--- Game/scripts/vscripts/heroes/axe/weapons/immortal_weapon_3.lua
+++ Game/scripts/vscripts/heroes/axe/weapons/immortal_weapon_3.lua
@@ -3,7 +3,7 @@ local function amplifyShieldsCount(caster)
 	--print("amplifyShieldsCount")
 	if caster:HasModifier("modifier_axe_immortal_weapon_3") then
 		local ability = caster:FindAbilityByName("red_general_ability_base_e_whirlwind")
-		CyclonicShield.red_general_rune_base_e_4_amplifyShieldsCount(caster, ability, 1 + RED_GENERAL_IMMORTAL_WEAPON_3_BONUS_SHIELDS_PERCENT / 100)
+		red_general_rune_base_e_4_amplifyShieldsCount(caster, ability, 1 + RED_GENERAL_IMMORTAL_WEAPON_3_BONUS_SHIELDS_PERCENT / 100)
 	end
 end
 
