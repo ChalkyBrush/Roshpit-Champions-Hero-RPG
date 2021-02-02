b9ddeed53d351381823ad84536e16e5e1dc951f7
8neame <43487693+8neame@users.noreply.github.com>
2021-02-02 20:28:31 +0800

    Glyph 3-2

---
+++ Game/scripts/vscripts/heroes/nightstalker/glyph_scripts/chernobog_glyph_3_2.lua
@@ -0,0 +1,58 @@
+require('items/lua/glyph/base_glyph')
+require('npc_abilities/base_modifier')
+require('heroes/nightstalker/chernobog_constants')
+
+item_rpc_chernobog_glyph_3_2 = class(BaseGlyph, nil, BaseGlyph)
+local itemClass = item_rpc_chernobog_glyph_3_2
+local itemClassName = 'item_rpc_chernobog_glyph_3_2'
+
+modifier_chernobog_glyph_3_2 = class(npc_base_modifier, nil, npc_base_modifier)
+local modifierClass = modifier_chernobog_glyph_3_2
+local modifierName = 'modifier_chernobog_glyph_3_2'
+LinkLuaModifier(modifierName, "heroes/nightstalker/glyph_scripts/chernobog_glyph_3_2", LUA_MODIFIER_MOTION_NONE)
+modifier_chernobog_glyph_3_2_cd = class(npc_base_modifier, nil, npc_base_modifier)
+LinkLuaModifier("modifier_chernobog_glyph_3_2_cd", "heroes/nightstalker/glyph_scripts/chernobog_glyph_3_2", LUA_MODIFIER_MOTION_NONE)
+
+function itemClass:GetModifierName()
+    return modifierName
+end
+
+function itemClass:GetItemName()
+    return itemClassName
+end
+
+------------
+--MODIFIER--
+------------
+
+function modifierClass:OnCreated()
+    if not IsServer() then
+        return
+    end
+end
+
+function modifierClass:IsHidden()
+    return true
+end
+function modifierClass:IsBuff()
+    return true
+end
+function modifierClass:RemoveOnDeath()
+    return false
+end
+
+function modifier_chernobog_glyph_3_2_cd:OnCreated()
+    if not IsServer() then
+        return
+    end
+end
+
+function modifier_chernobog_glyph_3_2_cd:IsHidden()
+    return true
+end
+function modifier_chernobog_glyph_3_2_cd:IsDebuff()
+    return true
+end
+function modifier_chernobog_glyph_3_2_cd:IsPurgable()
+    return false
+end
