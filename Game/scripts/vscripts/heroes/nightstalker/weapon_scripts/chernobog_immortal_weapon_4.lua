6d45992a03cd271d10b75c024b22162bf94b416f
8neame <43487693+8neame@users.noreply.github.com>
2021-02-02 20:23:28 +0800

    Chernobog Immortal Weapon 4

---
+++ Game/scripts/vscripts/heroes/nightstalker/weapon_scripts/chernobog_immortal_weapon_4.lua
@@ -0,0 +1,56 @@
+require('items/lua/weapon/base_weapon')
+require('npc_abilities/base_modifier')
+
+item_rpc_chernobog_immortal_weapon_4 = class(BaseWeapon, nil, BaseWeapon)
+local itemClass = item_rpc_chernobog_immortal_weapon_4
+local itemClassName = 'item_rpc_chernobog_immortal_weapon_4'
+
+modifier_chernobog_immortal_weapon_4 = class(npc_base_modifier, nil, npc_base_modifier)
+local modifierClass = modifier_chernobog_immortal_weapon_4
+local modifierName = 'modifier_chernobog_immortal_weapon_4'
+LinkLuaModifier(modifierName, "heroes/nightstalker/weapon_scripts/chernobog_immortal_weapon_4", LUA_MODIFIER_MOTION_NONE)
+
+function itemClass:RequiredHero()
+	return "chernobog"
+end
+
+function itemClass:GetClassName()
+    return itemClassName
+end
+
+function itemClass:GetName()
+    return 'Chernobog Immortal Weapon 4'
+end
+
+function itemClass:GetModifierName()
+    return modifierName
+end
+
+function itemClass:RollProperty1(item_level)
+	self.newItemTable.property1 = 1
+	self.newItemTable.property1name = "immortal_weapon_4"
+	RPCItems:SetPropertyValuesSpecial(self, "★", "#item_property_"..self:RequiredHero().."_immortal_weapon4", "#796DC6", 1, "#property_"..self:RequiredHero().."_immortal_weapon4_description")
+end
+
+function itemClass:RollProperty2(item_level)
+    Weapons:SetLegendWeaponProperty2(self, "base_ability", 2.5)
+end
+
+-- WEAPON MODIFIER
+function modifierClass:OnCreated()
+    if not IsServer() then
+        return
+    end
+end
+
+function modifierClass:IsHidden()
+    return true
+end
+
+function modifierClass:IsBuff()
+    return true
+end
+
+function modifierClass:RemoveOnDeath()
+    return false
+end
