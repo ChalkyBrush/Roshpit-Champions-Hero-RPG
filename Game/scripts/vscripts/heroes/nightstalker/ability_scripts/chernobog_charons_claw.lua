a9a211cb28c03bd52f22c7e50e17f4e96c62fc68
8neame <43487693+8neame@users.noreply.github.com>
2021-02-06 21:34:53 +0800

    Update chernobog_charons_claw.lua

--- Game/scripts/vscripts/heroes/nightstalker/ability_scripts/chernobog_charons_claw.lua
+++ Game/scripts/vscripts/heroes/nightstalker/ability_scripts/chernobog_charons_claw.lua
@@ -328,7 +328,7 @@ function modifier_charons_claw_on_path:OnCreated()
 	if not IsServer() then
 		return false
 	end
-	if ability:GetCaster() == self:GetParent() then
+	if ability:GetCaster():GetTeamNumber() == self:GetParent():GetTeamNumber() then
 		ability.allow_terrain_traverse = true
 		self:StartIntervalThink(0.03)
 	elseif self:GetParent():GetTeamNumber() ~= ability:GetCaster():GetTeamNumber() then
@@ -342,7 +342,7 @@ function modifier_charons_claw_on_path:CheckState()
 		return false
 	end
 	local state = {}
-	if ability:GetCaster() == self:GetParent() then
+	if ability:GetCaster():GetTeamNumber() == self:GetParent():GetTeamNumber() then
 		state = {
 			[MODIFIER_STATE_FLYING_FOR_PATHING_PURPOSES_ONLY] = ability.allow_terrain_traverse,
 		}
