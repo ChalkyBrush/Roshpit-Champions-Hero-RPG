6e66c17c7167c6b5e36809ae37f82c7a45f2718b
8neame <43487693+8neame@users.noreply.github.com>
2021-03-10 12:40:25 +0800

    make t4-1 glyph only calculate once
    Since its lua modifier

--- Game/scripts/vscripts/items/filters.lua
+++ Game/scripts/vscripts/items/filters.lua
@@ -2420,6 +2420,9 @@ function Filters:ElementalDamage(victim, attacker, damage, damage_type, slot, el
             end
         )
     end
+    if attacker:HasModifier("modifier_neutral_glyph_4_1") and #elements > 1 then
+	mult = mult - 0.5
+    end
     local newDamageCalculatorData = {
         victim = victim,
         attacker = attacker,
